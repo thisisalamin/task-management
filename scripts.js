e2437e943bd3437d7f3ca29bb7088a0d9dee2f53
thisisalamin <thisismdalamin@gmail.com>
2025-02-14 10:46:00 +0600

    Enhance submenu functionality and styles for improved user interaction

--- scripts.js
+++ scripts.js
@@ -48,6 +48,54 @@ $(document).ready(function() {
     $('.section:contains("In progress") .task-table tbody').append(newRow);
     closeTaskForm();
   });
+
+  // Remove old menu click handlers first
+  $('.nav-list > li > a').off('click');
+  $('.nav-list .sub-menu a').off('click');
+  $(document).off('click.submenu');
+
+  // Add new menu click handler
+  $('.nav-list > li > a').on('click', function(e) {
+    if ($(this).siblings('.sub-menu').length) {
+      e.preventDefault();
+      e.stopPropagation();
+      
+      // Toggle only this submenu
+      const $submenu = $(this).siblings('.sub-menu');
+      const $icon = $(this).find('.fa-chevron-right, .fa-chevron-down');
+      
+      // Don't close if already open
+      if (!$submenu.is(':visible')) {
+        // Close other submenus
+        $('.nav-list > li > a').not(this).siblings('.sub-menu').slideUp(300);
+        $('.nav-list > li > a').not(this).find('.fa-chevron-down')
+          .removeClass('fa-chevron-down')
+          .addClass('fa-chevron-right');
+      }
+      
+      // Toggle current submenu
+      $submenu.slideToggle(300);
+      $icon.toggleClass('fa-chevron-right fa-chevron-down');
+    }
+  });
+
+  // Make submenu items clickable without closing
+  $('.nav-list .sub-menu a').on('click', function(e) {
+    e.stopPropagation();
+  });
+
+  // Add this new handler for clicking anywhere else on the page
+  $(document).click(function(e) {
+    if (!$(e.target).closest('.nav-list').length) {
+      $('.sub-menu').slideUp(300);
+      $('.nav-list li a .fa-chevron-down').removeClass('fa-chevron-down').addClass('fa-chevron-right');
+    }
+  });
+
+  // Prevent submenu items from closing the submenu when clicked
+  $('.nav-list .sub-menu a').click(function(e) {
+    e.stopPropagation();
+  });
 });
 
 // Add close notification functionality
